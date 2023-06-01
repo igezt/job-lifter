44b2beb92c1221b326af19f0dd0889141c31cadb
igezt <pierce.nws@gmail.com>
2023-06-02 00:47:23 +0800

    Initial setup and extra components definition.

---
+++ job-lifter/app/index.js
@@ -0,0 +1,44 @@
+import { ScrollView, Text, View, SafeAreaView } from "react-native";
+import { useState } from "react";
+import {Stack, useRouter} from 'expo-router';
+
+import { COLORS, icons, images, SIZES} from '../constants';
+
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components';
+
+export default function Page() {
+
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}} >
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor:COLORS.lightWhite},
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+          ), 
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%"/>
+          ),
+          headerTitle: ""
+        }}
+      />
+      <ScrollView>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium
+          }}
+        >
+          <Welcome />
+
+          <Popularjobs/>
+          <Nearbyjobs/>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
